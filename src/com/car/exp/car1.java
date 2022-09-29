4af3bf48bdebfc2d43a4eb0aa6587cd26dd48df2
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-09-29 14:39:43 +0400

    Obyekt deyisenleri

--- src/com/car/exp/car1.java
+++ src/com/car/exp/car1.java
@@ -1,2 +1,18 @@
-package com.car.exp;public class car1 {
+package com.car.exp;
+
+public class car1 {
+    int hp;
+    boolean awd;
+
+    public static void main(String[] args) {
+        car1 bmw = new car1();
+        bmw.hp = 286;
+        bmw.awd = false;
+        if (bmw.awd == true) {
+            System.out.println("the power of e90 335d is " + bmw.hp + " hp, all wd");
+        }
+        else {
+            System.out.println("the power of e90 335d is " +bmw.hp + " hp, rear wd");
+        }
+    }
 }

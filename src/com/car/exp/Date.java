fda69d67715483924b530814c75767c83e55d43d
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-09-29 15:06:09 +0400

    Date class

---
+++ src/com/car/exp/Date.java
@@ -0,0 +1,12 @@
+package com.car.exp;
+
+
+public class Date {
+
+    public static void main(String[] args) {
+       Date tarix = new Date();
+       // java.sql.Date a= new java.sql.Date();
+
+        System.out.println(tarix);
+    }
+}

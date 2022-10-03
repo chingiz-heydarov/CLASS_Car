d2223c25e8ceebbcae92b92d434eee54347a37d4
arsenliliyev <114281321+arsenliliyev@users.noreply.github.com>
2022-10-03 18:48:53 +0400

    Update Task2.java

--- src/com/car/exp/Task2.java
+++ src/com/car/exp/Task2.java
@@ -1,24 +1,6 @@
-package com.car.exp;
 
-import java.util.Scanner;
 
-public class Task2 {
-    public static void main(String[] args) {
-        int b;
-        do {
-            System.out.println("---------Enter the value: ");
-            Scanner console = new Scanner(System.in);
-            int a = console.nextInt();
-            b=a;
-            if (a > 0 && a % 2 == 0) System.out.println("MUSBET, CUT");
-            else if (a > 0 ) System.out.println("MUSBET, TEK");
-            else if (a < 0 && a % 2 != 0) System.out.println("MENFI, TEK");
-            else if (a < 0 ) System.out.println("MENFI, CUT");
-            else System.out.println("reqem sifirdir");
-        }while (b!=0);
-    }
-
-    public static class Task1_october_2 {
+   public static class Task1_october_2 {
 
 
             public static void main (String[]args) {

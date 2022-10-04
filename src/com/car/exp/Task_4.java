d78bd82ffc3e528a4ea7b9398568b48f27c80b62
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-04 10:53:27 +0400

    task 2   octyabr 2

--- src/com/car/exp/Task_4.java
+++ src/com/car/exp/Task_4.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 
 public class Task_4 {
     public static void main(String[] args) {
-
         System.out.println("---------Enter the value: ");
             Scanner console = new Scanner(System.in);
             int a = console.nextInt();
             if(1<=a && a<=999) {
                 int x, y, z;
                 int w = 1;
-                while (w > 0) {
+                int c=0;
+                while (c < 100) {
                     x = a / 100;
                     y = (a % 100) / 10;
                     z = a % 10;
                     w = x * x + y * y + z * z;
+                    System.out.println(w);
                     if (w % 10 == 1) {
                         System.out.println("true");
                         break;
                     } else a = w;
+                    c++;
                 }
+                if(c==100) System.out.println("false");
             }
             else System.out.println("false");
     }

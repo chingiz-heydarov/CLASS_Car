22d515b435288ed856d5ff194b7c5b8accfec6d2
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-04 16:02:36 +0400

    task 3   octyabr 2

---
+++ src/com/car/exp/Task_3.java
@@ -0,0 +1,40 @@
+package com.car.exp;
+
+import java.util.Scanner;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        System.out.println("---------Select operation (+,-,*,/): ");
+        Scanner sign = new Scanner(System.in);
+        var o = sign.nextLine();
+
+           System.out.print("a=");
+
+        Scanner a = new Scanner(System.in);
+        int x = a.nextInt();
+
+           System.out.print("b=");
+
+        Scanner b = new Scanner(System.in);
+        int y = b.nextInt();
+
+        int c = 0;
+
+        switch (o) {
+            case "+":
+                c = x + y;
+                break;
+            case "-":
+                c = x - y;
+                break;
+            case "*":
+                c = x * y;
+                break;
+            case "/":
+                c = x / y;
+                break;
+        }
+        System.out.println(c);
+    }
+}
+

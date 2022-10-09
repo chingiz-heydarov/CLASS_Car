68dc8b3104d6c0e367ed777b321b73a37961a92a
chingiz <cinqiz.qeydarov@gmail.com>
2022-10-09 10:49:20 +0400

    ders tapsiriq

---
+++ src/com/car/exp/EightAndFive.java
@@ -0,0 +1,9 @@
+package com.car.exp;
+
+public class EightAndFive {
+    public static void main(String[] args) {
+        int a=5;int b=8;
+        b=(a*b)/b;
+        System.out.println(b);
+    }
+}

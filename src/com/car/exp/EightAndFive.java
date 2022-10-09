b08a6522ef0a5cddf76953d63daba268306e8563
chingiz <cinqiz.qeydarov@gmail.com>
2022-10-09 10:52:29 +0400

    ders tapsirigi

--- src/com/car/exp/EightAndFive.java
+++ src/com/car/exp/EightAndFive.java
@@ -1,9 +1,16 @@
 package com.car.exp;
 
+import java.util.Scanner;
+
 public class EightAndFive {
     public static void main(String[] args) {
-        int a=5;int b=8;
+        System.out.print("a=");
+        Scanner reqem = new Scanner(System.in);
+        int a = reqem.nextInt();
+        System.out.print("b=");
+        Scanner reqem2 = new Scanner(System.in);
+        int b = reqem2.nextInt();
         b=(a*b)/b;
-        System.out.println(b);
+        System.out.println("b="+b);
     }
 }
--- src/com/car/exp/SevenThree.java
+++ src/com/car/exp/SevenThree.java
@@ -6,11 +6,11 @@ public class SevenThree {
     public static void main(String[] args) {
 
         System.out.println("Birinci reqemi daxil edin");
-            Scanner reqem = new Scanner(System.in);
-            int a = reqem.nextInt();
-            System.out.println("Ikinci reqemi daxil edin");
-            Scanner reqem2 = new Scanner(System.in);
-            int b = reqem2.nextInt();
+        Scanner reqem = new Scanner(System.in);
+        int a = reqem.nextInt();
+        System.out.println("Ikinci reqemi daxil edin");
+        Scanner reqem2 = new Scanner(System.in);
+        int b = reqem2.nextInt();
             if(a<b) {
                 for (int i = a; i < b; i++)
                     if (i % 21 == 0) System.out.println(i);

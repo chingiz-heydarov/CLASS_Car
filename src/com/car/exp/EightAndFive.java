a3d9409d1f49c7543e544d7f6183d67f2529611e
SHAIG JAFAROV <shaiq.jafarov@abb-bank.az>
2022-10-09 11:32:03 +0400

    review

--- src/com/car/exp/EightAndFive.java
+++ src/com/car/exp/EightAndFive.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 
 public class EightAndFive {
     public static void main(String[] args) {
-      System.out.print("a=");
+        System.out.print("a=");
         Scanner reqem = new Scanner(System.in);
         int a = reqem.nextInt();
         System.out.print("b=");
         Scanner reqem2 = new Scanner(System.in);
         int b = reqem2.nextInt();
 
-       a=a*b;
-        b=a/b;
-        a=a/b;
-        System.out.println("b="+b);
-        System.out.println("a="+a);
+        a = a * b;//24
+        b = a / b;//3
+        a = a / b;//8
+        System.out.println("b=" + b);
+        System.out.println("a=" + a);
 
     }
 }
--- src/com/car/exp/SimpleCount.java
+++ src/com/car/exp/SimpleCount.java
@@ -1,17 +1,17 @@
 package com.car.exp;
 
 public class SimpleCount {
-    public static void main(String[] args){
-        int a=1;
-        while(a<100){
-            int b=2;
-            if(a<=b) System.out.println(a);
-            while(b<a){
-                if(a-b==1)  System.out.println(a);
-                if(a%b!=0){
+
+    public static void main(String[] args) {
+        int a = 1;
+        while (a < 100) {
+            int b = 2;
+            if (a <= b) System.out.println(a);
+            while (b < a) {
+                if (a - b == 1) System.out.println(a);
+                if (a % b != 0) {
                     b++;
-                }
-                else break;
+                } else break;
             }
             a++;
         }

69edb71e9781335ccf56f9be1bb1ef564145f77b
chingiz <cinqiz.qeydarov@gmail.com>
2022-10-09 11:06:09 +0400

    ders tapsirigii7

--- src/com/car/exp/EightAndFive.java
+++ src/com/car/exp/EightAndFive.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 
 public class EightAndFive {
     public static void main(String[] args) {
-        System.out.print("a=");
+      System.out.print("a=");
         Scanner reqem = new Scanner(System.in);
         int a = reqem.nextInt();
         System.out.print("b=");
         Scanner reqem2 = new Scanner(System.in);
         int b = reqem2.nextInt();
-        b=(a*b)/b;
+
+       a=a*b;
+        b=a/b;
+        a=a/b;
         System.out.println("b="+b);
+        System.out.println("a="+a);
+
     }
 }

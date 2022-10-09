15b2b1141a732663e626f937b4d48500f256bfaf
chingiz <cinqiz.qeydarov@gmail.com>
2022-10-09 10:28:43 +0400

    8 oktyabr 2ci tapsiriq tezbazar

--- src/com/car/exp/SevenThree.java
+++ src/com/car/exp/SevenThree.java
@@ -4,17 +4,14 @@ import java.util.Scanner;
 
 public class SevenThree {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Birinci reqemi daxil edin");
+
+        System.out.println("Birinci reqemi daxil edin");
             Scanner reqem = new Scanner(System.in);
             int a = reqem.nextInt();
             System.out.println("Ikinci reqemi daxil edin");
             Scanner reqem2 = new Scanner(System.in);
             int b = reqem2.nextInt();
-            if (a % 21 == 0) System.out.println("a 7 ve 3 bolunur");
-            else System.out.println("a 7 ve 3 bolunMUR");
-            if (b % 21 == 0) System.out.println("b 7 ve 3 bolunur");
-            else System.out.println("b 7 ve 3 bolunMUR");
+            for(int i=a; i<b; i++)
+            if (i % 21 == 0) System.out.println(i);
         }
     }
-}

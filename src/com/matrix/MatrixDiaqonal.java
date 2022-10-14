b4bc4639c21bf298c793026c2c19194846a873f6
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-14 15:45:18 +0400

    Oktyabr 14 Matrisa Diaqonal (Task-2)

--- src/com/matrix/MatrixDiaqonal.java
+++ src/com/matrix/MatrixDiaqonal.java
@@ -3,11 +3,13 @@ package com.matrix;
 public class MatrixDiaqonal {
     public static void main(String[] args) {
         int a[][]={{1,3,7,6,1},{8,1,2,1,5},{3,5,1,9,6},{2,1,3,1,0},{1,4,7,3,1}};
+
         /*     1  3  7  6  1
                8  1  2  1  5
                3  5  1  9  6
                2  1  3  1  0
                1  4  7  3  1    */
+
         int length=a.length-1;
         int primary=0, secondary=0;
         for(int i=0,j=0; i<=length; i++,j++){

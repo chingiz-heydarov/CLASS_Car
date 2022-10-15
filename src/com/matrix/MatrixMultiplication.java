520d261921396984fccfe0387e307937fa922373
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-15 18:43:55 +0400

    Oktyabr 14 Matrix Multiplication HAZIR

--- src/com/matrix/MatrixMultiplication.java
+++ src/com/matrix/MatrixMultiplication.java
@@ -26,8 +26,8 @@ public class MatrixMultiplication {
         }
 
         System.out.println();
-        System.out.println("A matrisasi:");
-        System.out.println();
+        System.out.println("A matrisasi:"+"\n");
+
 
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
@@ -36,9 +36,8 @@ public class MatrixMultiplication {
             System.out.println();
         }
         System.out.println();
-        System.out.println("A length= " + a[0].length);
-        System.out.println("A width= " + a.length);
-        System.out.println();                                      //...bura qeder A matrisasi daxil olunur ve consola cixarilir
+        System.out.println("A length= " + a[0].length+"\n");
+        System.out.println("A width= " + a.length+"\n");         //...bura qeder A matrisasi daxil olunur ve consola cixarilir
 
         // Burdan...
         System.out.println("QEYD: ikinci (B) matrisanin uzunlugu birinci (A) matrisanin enliyi qeder ola biler, enliyi uzunluga qeder");
@@ -53,8 +52,7 @@ public class MatrixMultiplication {
         }
 
         System.out.println();
-        System.out.println("B matrisasi:");
-        System.out.println();
+        System.out.println("B matrisasi:"+"\n");
 
         for (int i = 0; i < y; i++) {
             for (int j = 0; j < x; j++) {
@@ -64,48 +62,41 @@ public class MatrixMultiplication {
         }
         System.out.println();
         System.out.println("B length= " + b[0].length);
-        System.out.println("B width= " + b.length);               //...bura qeder B matrisasi doldurulur ve consola cixarilir
-        System.out.println();
+        System.out.println("B width= " + b.length+"\n");               //...bura qeder B matrisasi doldurulur ve consola cixarilir
         System.out.println("Cavab matrisasi (C):");
 
-        int[][] c;
+        int[][] c;                                              // C matrisa elan olunur
         c = new int[x][x];
-        //else c = new int[y][y];
 
-        // int length = a.length - 1;
-        // int width = a.length-1;   //  =2
-
-        // System.out.println("width="+width);
-        System.out.println("y=" + y);
-
-        if (x > y) {
-            for (int i = 0; i <= a.length - 1; i++) {
-                for (int j = 0; j <= a.length - 1; j++) {
+        for (int i = 0; i <= a.length - 1; i++) {                         // A ve B matrisalarin muxtelif formalari nezere tutulur
+            for (int j = 0; j <= a.length - 1; j++) {
+                if (x > y) {
                     for (int k = 0; k < a.length - 1; k++) {
+                        c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                    }
+                }
+                else if(x<y){
+                    for (int k = 0; k <= a.length; k++) {
+                        c[i][j] = c[i][j] + a[i][k] * b[k][j];
+                    }
+                }
+                else{
+                    for (int k = 0; k <= a.length-1; k++) {
                         c[i][j] = c[i][j] + a[i][k] * b[k][j];
                     }
                 }
             }
         }
-
-        else{
-                for (int i = 0; i <= a.length-1; i++) {
-                    for (int j = 0; j <= a.length-1; j++) {
-                        for (int k = 0; k <= a.length; k++) {
-                            c[i][j] = c[i][j] + a[i][k] * b[k][j];
-                        }
-                    }
-                }
+        for (int i = 0; i <= a.length - 1; i++) {                     // Matrisa cixisa verilir
+            for (int j = 0; j <= a.length - 1; j++) {
+                System.out.print(c[i][j] + " ");
             }
-
-            for (int i = 0; i <= a.length - 1; i++) {
-                for (int j = 0; j <= a.length - 1; j++) {
-                    System.out.print(c[i][j] + " ");
-                }
-                System.out.println();
-            }
+            System.out.println();
         }
     }
+}
+
+
 
 
 

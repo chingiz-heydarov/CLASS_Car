476a0d77c0570d2569758db831baa40c0fa80686
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-19 15:54:12 +0400

    Oktyabr 19 Saat 180 qirdim

--- src/com/audi/Saat.java
+++ src/com/audi/Saat.java
@@ -11,15 +11,30 @@ public class Saat {
             System.out.println("---------Enter minutes: ");
             Scanner consol = new Scanner(System.in);
             int min = consol.nextInt();
-            int m = min / 5, angle = 0;
+            int m =  min / 5, angle = 0;
             if (h > m) {
                 if (h - m < 6) angle = min / 2 - 6 * (min % 5) + 30 * (h - m);
                 else if (h - m > 6) angle = 360 - min / 2 + 6 * (min % 5) + 30 * (m - h);
+                else {
+                    if(6*(min%5) > min/2) angle = 180-6*(min%5)+min/2;
+                    else if (6*(min%5) < min/2) angle=180-min/2+6*(min%5);
+                    else angle=180;
+                }
             } else if (h < m) {
                 if (m - h > 6) angle = 360 + min / 2 - 6 * (min % 5) + 30 * (h - m);
                 else if (m - h < 6) angle = 30 * (m - h) + 6 * (min % 5) - min / 2;
+                else {
+                    if(6*(min%5) > min/2) angle = 180-6*(min%5)+min/2;
+                    else if (6*(min%5) < min/2) angle=180-min/2+6*(min%5);
+                    else angle=180;
+                }
             }
-            System.out.println(angle);
+            else{                                             // h==m
+                if(min/2 > 6*(min%5)) angle=min/2-6*(min%5);
+                else if(min/2 < 6*(min%5)) angle=6*(min%5)-min/2;
+                else angle=0;
+            }
+            System.out.println("angle="+angle);
         }
     }
 }

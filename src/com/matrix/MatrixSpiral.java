3f8468f9fb50b31fea9a8d3c2b8d3ee31ef55fcc
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-19 15:54:30 +0400

    Oktyabr 19 Saat 180 qirdim

--- src/com/matrix/MatrixSpiral.java
+++ src/com/matrix/MatrixSpiral.java
@@ -6,10 +6,10 @@ public class MatrixSpiral {
         int a[][] = {{1, 2, 3, 4}, {10, 20, 30, 40}, {100, 200, 300, 400}, {1000, 2000, 3000, 4000}};
         int len=a.length-1, sum=0, i, j;
 
-        for( j=0, i=0; j<=len; j++) {
-            System.out.println("a["+i+"]["+j+"]="+a[i][j]);
-            sum+=a[i][j];
-        }
+             for( j=0, i=0; j<=len; j++) {
+               System.out.println("a["+i+"]["+j+"]="+a[i][j]);
+               sum+=a[i][j];
+             }
         for (int x = 1; x <= a.length/2; x++){
             int m=x-1;
            for(i=x, j=len-m; i<=len-m; i++) {

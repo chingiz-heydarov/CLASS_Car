7d974f59d9ca533eb10c8e624647e09ab5089d6a
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-25 13:22:35 +0400

    Oktyabr 25 Task-1 String variant qeribe

--- src/com/oktyabr25/Task1.java
+++ src/com/oktyabr25/Task1.java
@@ -2,22 +2,26 @@ package com.oktyabr25;
 
 public class Task1 {
     public static void main(String[] args) {
-        int a[]= {9,9,9,8,9};
+        int a[]= {9,9,9,9,9};
         int i,l=a.length-1,sum=0;
         for(i=0;i<=l;i++) {
             sum+=a[i]*(Math.pow(10,l-i));
         }
         sum+=1;
-        if(sum==Math.pow(10,l+1)) l+=1;
 
-        int b[]=new int[l+1];
+        String s=""+sum;
+
+       if(sum==Math.pow(10,l+1)) l+=1;
+
+    //    char b[]=new char[l+1];
 
         for(i=0;i<=l;i++){
-             b[i]=sum/(int) Math.pow(10,l-i);
-             sum=sum%((int) Math.pow(10,l-i));
+          //   b[i]= s.charAt(i);
+               System.out.print(s.charAt(i)+"\t");
+         //   System.out.println(b[i]);
         }
-        for( int z: b){
+  /*      for( int z: b){
             System.out.print(z+"\t");
+        }                                  */
         }
-        }
-    }
+   }

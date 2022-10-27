fafb690d0381278523b3ae1757c793289286672a
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-27 18:17:55 +0400

    Oktyabr 27 natamam metodlu variant

--- src/com/oktyabr27/StringMethod.java
+++ src/com/oktyabr27/StringMethod.java
@@ -2,27 +2,25 @@ package com.oktyabr27;
 
 public class StringMethod {
     public static void main(String[] args) {
-        String w[]={"unt","er","gehen"};
-        String st="untergehen";
-        int i=0,j=0,x,y,a=0,k,total=0, Stl=st.length();
+        String w[] = {"unt", "er", "gehen"};
+        String st = "untergehen";
+        System.out.println(word(st, w));
+    }
 
-        for(x=0;x<w.length;x++){
-            int l=w[x].length();
-            total+=l;
-        }
-        if(total<Stl) Stl=total;
+    static boolean word(String st, String w[]) {
 
-     outer:   while(i!=Stl){
-            int l=w[j].length();
-            for(i=a,k=0;k<l;i++,k++){
-               if(st.charAt(i)!=w[j].charAt(k)) break outer;
-               // System.out.print(w[j].charAt(k)+"\t");
+        int i = 0, j = 0, a = 0, k, Stl = st.length();
+
+        outer:   while (i != Stl) {
+            int l = w[j].length();
+            for (i = a, k = 0; k < l; i++, k++) {
+                if (st.charAt(i) != w[j].charAt(k)) break outer;
+                // System.out.print(w[j].charAt(k)+"\t");
             }
-            a+=l;
+            a += l;
             j++;
         }
-
-        if(i==Stl) System.out.println("true");
-        else System.out.println("false");
+        if (i == Stl) return true;
+        else return false;
     }
 }

52922f8712223f0d1981885b3783761997fbe268
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-10-29 16:54:28 +0400

    Oktyabr 29 tam ideal deyil hele ki

---
+++ src/com/oktyabr29/StringMetod.java
@@ -0,0 +1,20 @@
+package com.oktyabr29;
+
+public class StringMetod {
+    public static void main(String[] args) {
+        String s[] = {"Au","di","av", "aav","Av","ant"};
+        String str = "AudiAvantAv", sum;
+        int i,j,l=s.length-1;
+  outer:   for(i=0;i<l-1;i++){
+                sum=s[i];
+                for(j=i+1;j<=l;j++){
+                        if((str.substring(0,(sum.length()+s[j].length()))).equals(sum+s[j])) sum+=s[j];
+                        if(str.equals(sum)){
+                            System.out.println("true");
+                            break outer;
+                        }
+            }
+                if(i==l-2)System.out.println("false");
+        }
+    }
+}

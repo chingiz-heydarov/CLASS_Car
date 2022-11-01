730c62250994d02a88fd9c24dada91e24836bad5
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-11-01 16:41:27 +0400

    Oktyabr 31 hazir

--- src/com/october31/LongestEntrance.java
+++ src/com/october31/LongestEntrance.java
@@ -2,7 +2,12 @@ package com.october31;
 
 public class LongestEntrance {
     public static void main(String[] args) {
+
         String s = "audi_rs6_audi_s_8_audi_s_5_audi_rsq_5";
+        result(s, (arraySize(s)));
+    }
+
+    public static int arraySize(String s) {
 
         int size = 0;
 
@@ -13,37 +18,40 @@ public class LongestEntrance {
                 }
             }
         }
+        return size;
+    }
 
+    public static void result(String s, int size) {
         String[] sb = new String[size];
-        int k=0;
+        int k = 0;
 
         for (int l = 2; l <= s.length() / 2; l++) {
             for (int i = 0; i < l; i++) {
                 for (int j = i; j <= s.length() - l; j += l) {
-                sb[k]=s.substring(j,j+l);
-                k++;
+                    sb[k] = s.substring(j, j + l);
+                    k++;
                 }
             }
         }
-        int x=0,c=0;
+        int maxI = 0, oldC = 0;
 
 
-        for(int i=0; i<sb.length-1; i++) {
+        for (int i = 0; i < sb.length - 1; i++) {
             int count = 0;
             for (int j = i + 1; j < sb.length; j++) {
                 if (sb[i].equals(sb[j])) count++;
             }
-            if (count > c) {
-                c = count;
-                x = i;
+            if (count > oldC) {
+                oldC = count;
+                maxI = i;
             }
-            if(count==c){
-                if(sb[i].length()>sb[x].length()){
-                    c=count;
-                    x=i;
+            if (count == oldC) {
+                if (sb[i].length() > sb[maxI].length()) {
+                    oldC = count;
+                    maxI = i;
                 }
             }
         }
-        System.out.println("'"+sb[x]+"'"+" soz birlesmesi "+(c+1)+" defe tekrerlanir");
+        System.out.println("'" + sb[maxI] + "'" + " soz birlesmesi " + (oldC + 1) + " defe tekrerlanir.");
     }
 }

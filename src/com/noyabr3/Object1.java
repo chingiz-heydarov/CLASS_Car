20e320fa7854c99593a0244c5b36e1a5f1dde8a8
arsenliliyev <cinqiz.qeydarov@gmail.com>
2022-11-03 12:00:20 +0400

    Noyabr 3 hazir

--- src/com/noyabr3/Object1.java
+++ src/com/noyabr3/Object1.java
@@ -3,40 +3,28 @@ package com.noyabr3;
 public class Object1 {
     public static void main(String[] args) {
 
-        car[] massiv = new car[5];
+        String[] vehicle = {"vw passat", "seat leon", "vw jetta", "vw golf", "skoda octavia"};
+        int[] power = {220, 180, 170, 235, 185};
+        car[] massiv = new car[power.length];
 
-        massiv[0] = new car("vw passat", 220);
+   /*   massiv[0] = new car("vw passat", 220);
         massiv[1] = new car("seat leon", 180);
         massiv[2] = new car("vw jetta", 170);
         massiv[3] = new car("vw golf", 235);
-        massiv[4] = new car("skoda octaiva", 185);
+        massiv[4] = new car("skoda octaiva", 185);  */
 
-    /*
-            car passat = new car();
-            passat.model = "alltrack_b8";
-            passat.hp = 220;
 
-            car seat = new car();
-            seat.model = "leon";
-            seat.hp = 180;
-
-            car jetta = new car();
-            jetta.model = "GLI";
-            jetta.hp = 211;
-
-            car golf = new car();
-            golf.model = "R_mk7";
-            golf.hp = 310;
-
-            car skoda = new car();
-            skoda.model = "octavia";
-            skoda.hp = 185;   */
+        for (int i = 0; i < massiv.length; i++) {
+            massiv[i] = new car();
+            massiv[i].model = vehicle[i];
+            massiv[i].hp = power[i];
+        }
 
         int count;
 
         do {
             count = 0;
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < massiv.length-1; i++) {
                 if (massiv[i].hp > massiv[i + 1].hp) {
                     car b = massiv[i];
                     massiv[i] = massiv[i + 1];
@@ -47,13 +35,10 @@ public class Object1 {
         } while (count != 0);
 
         for (int i = 0; i < 5; i++) {
-            System.out.println(massiv[i].model+" "+massiv[i].hp);
-
+            System.out.println(massiv[i].model + " " + massiv[i].hp);
         }
-
     }
-
-    }
+}
 
 
 class car {
@@ -61,13 +46,13 @@ class car {
     int hp;
 
 
-    public car(String x, int y) {
+ /*   public car(String x, int y) {
         this.model = x;
         this.hp = y;
     }
 
 
-  /*
+
   static int count;
 
    static int i=0;

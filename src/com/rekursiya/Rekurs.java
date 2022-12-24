03b6f6cc7602a8502d26abacbc93fd1406f8bb45
chingiz <cinqiz.qeydarov@gmail.com>
2022-12-24 23:18:50 +0400

    rekursiya

---
+++ src/com/rekursiya/Rekurs.java
@@ -0,0 +1,28 @@
+package com.rekursiya;
+
+import java.util.Scanner;
+
+public class Rekurs {
+    public static void main(String[] args) {
+
+        while(true) {
+            System.out.println("reqem daxil edin: ");
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+
+            System.out.println(faktorial(x));
+        }
+    }
+
+    static int faktorial(int z){
+        if(z<0) {
+            System.out.println("wrong entry");
+            return -1;
+        }
+        if(z==1 || z==0){
+            return z;
+
+        }
+        return z*faktorial(z-1);
+    }
+}

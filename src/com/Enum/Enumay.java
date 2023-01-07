ad2c162be7de4b412c4e911138c5eb5303d31169
chingiz <cinqiz.qeydarov@gmail.com>
2023-01-08 00:00:44 +0400

    yanvar 7

---
+++ src/com/Enum/Enumay.java
@@ -0,0 +1,84 @@
+package com.Enum;
+
+import com.task.Task1October2;
+
+import java.util.Scanner;
+
+public class Enumay {
+    public static void main(String[] args) {
+        while (true) {
+
+            int ay;
+
+            while (true) {
+                System.out.println("Ayin nomresini daxil edin: ");
+                Scanner scn = new Scanner(System.in);
+                ay = scn.nextInt();
+                if (ay > 0 && ay < 13) break;
+                else System.out.println("Bu nomreli ay movcud deyil.");
+            }
+
+
+            System.out.println(" EN/AZ/RU/TR formatinda dili secib daxil edin: ");
+            Scanner sc = new Scanner(System.in);
+            Language dil = Language.valueOf(sc.nextLine());
+
+            Month[] mm = Month.values();
+
+            for (Month z : mm) {
+                if (z.number == ay) {
+                    switch (dil) {
+                        case EN:
+                            System.out.println(z);
+                            break;
+                        case AZ:
+                            System.out.println(z.az);
+                            break;
+                        case RU:
+                            System.out.println(z.ru);
+                            break;
+                        case TR:
+                            System.out.println(z.tr);
+                            break;
+                    }
+                    System.out.println("---------------------------------------------------------");
+                }
+            }
+        }
+
+    }
+}
+
+enum Language {
+    AZ,
+    EN,
+    TR,
+    RU
+}
+
+enum Month {
+    JANUARY(1, "Yanvar", "Январь", "Ocak"),
+    FEBRUARY(2, "Fevral", "Февраль", "Subat"),
+    MARCH(3, "Mart", "Март", "Mart"),
+    APRIL(4, "Aprel", "Апрель", "Nisan"),
+    MAY(5, "May", "Май", "Mayis"),
+    JUNE(6, "Iyun", "Июнь", "Haziran"),
+    JULY(7, "Iyul", "Июль", "Temmuz"),
+    AUGUST(8, "Avqust", "Август", "Agustos"),
+    SEPTEMBER(9, "Sentyabr", "Сентябрь", "Eylul"),
+    OCTOBER(10, "Oktyabr", "Октябрь", "Ekim"),
+    NOVEMBER(11, "Noyabr", "Ноябрь", "Kasim"),
+    DECEMBER(12, "Dekabr", "Декабрь", "Aralik");
+
+    int number;
+    String az, ru, tr;
+
+
+    Month(int number, String az, String ru, String tr) {
+        this.number = number;
+        this.az = az;
+        this.ru = ru;
+        this.tr = tr;
+    }
+
+}

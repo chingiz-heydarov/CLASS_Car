1e11d3edee5134056dd35082c67f46093fe33a6b
chingiz <cinqiz.qeydarov@gmail.com>
2023-01-15 11:44:12 +0400

    yanvar 15+

--- src/com/yanvar15ders/Man2.java
+++ src/com/yanvar15ders/Man2.java
@@ -2,29 +2,31 @@ package com.yanvar15ders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Man2 {
     public static void main(String[] args) {
 
-        List<Integer> a=new ArrayList<>();
+        List<Integer> a = new ArrayList<>();
+
         a.add(1);
         a.add(2);
         a.add(3);
         a.add(2);
         a.add(4);
 
-        List<Integer> b=new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
         b.add(2);
         b.add(6);
         b.add(4);
         b.add(1);
         b.add(2);
 
-        Object[] c =a.stream().filter(b::contains).sorted().distinct().toArray();
-        for (Object z: c
-             ) {
-            System.out.println(z);
 
+        int c[] = a.stream().flatMapToInt(IntStream::of).filter(b::contains).sorted().distinct().toArray();
+
+        for (Object z : c) {
+            System.out.println(z);
         }
     }
 }

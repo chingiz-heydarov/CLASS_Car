76e2a6952d52d53dad60fe4d17e431f27dbb9cea
SHAIG JAFAROV <shaiq.jafarov@abb-bank.az>
2023-01-15 11:33:26 +0400

    push remont

--- src/com/yanvar15ders/Man.java
+++ src/com/yanvar15ders/Man.java
@@ -2,14 +2,19 @@ package com.yanvar15ders;
 
 import java.util.IntSummaryStatistics;
 import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Man {
+    static int a=5;
     public static void main(String[] args) {
         Random random = new Random();
 
         //IntSummaryStatistics a=random.ints(100).limit(10).summaryStatistics();
 
         System.out.println(random.ints(100).limit(10).summaryStatistics());
+
+        IntSummaryStatistics intSummaryStatistics = Stream.generate(() -> a++).limit(10).mapToInt(a->Integer.parseInt("12")).summaryStatistics();
 
     /*    double average=random.ints(100).limit(10).summaryStatistics().getAverage();
         System.out.println("aver= "+average);

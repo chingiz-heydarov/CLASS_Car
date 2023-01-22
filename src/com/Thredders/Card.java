c644fb8c64e159a1e72eab781a6b3df855175814
chingiz <cinqiz.qeydarov@gmail.com>
2023-01-22 11:18:51 +0400

    yanvar 22 ders

--- src/com/Thredders/Card.java
+++ src/com/Thredders/Card.java
@@ -7,10 +7,11 @@ public class Card {
     String cardNumber;
     int balance;
     LocalDate expDate;
-    Card(String holderName, String cardNumber, int balance, LocalDate expDate){
-        this.balance=balance;
-        this.cardNumber=cardNumber;
-        this.expDate=expDate;
-        this.holderName=holderName;
+
+    Card(String holderName, String cardNumber, int balance, LocalDate expDate) {
+        this.balance = balance;
+        this.cardNumber = cardNumber;
+        this.expDate = expDate;
+        this.holderName = holderName;
     }
 }
